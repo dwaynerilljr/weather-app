873447c6a5642624ea93ef79b793788f256b0cd8
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2023-01-03 13:52:18 -0500

    Grabbed data from API - Wrote an async function to grab city data from AccuWeather API - Wrote another async function that takes city data and grabs current conditions from API

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,23 @@
 const key = 'w1bhlGDLgLjXGCotZIoYt09m7TK0WHsF';
+
+// Get current weather
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const res = await fetch(base + query);
+    const data = await res.json();
+
+    return data[0];
+}
+
+// Get city info
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const res = await fetch(base + query);
+    const data =  await res.json();
+
+    return data[0];
+};
